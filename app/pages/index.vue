4212f151887872610a19ac2f7d554dc71a671ac8
shaquillekiragu <skiragu77@gmail.com>
2026-02-02 09:53:14 +0000

    setup repo

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="w-screen min-h-screen flex flex-col items-center my-10">
+        <h1 class="text-4xl font-bold text-center">UCCF Christian Unions</h1>
+    </main>
+</template>
